c51033d2674c7b4817dac5c842ecae3039f1ce09
hasithaDa <hamdhasith@gmail.com>
2025-01-27 18:19:15 +0530

    Databse setup,signup and login & logout and Update profile

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+// Initialize express
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log("Server is running on PORT:" + PORT);
+  connectDB();
+});
