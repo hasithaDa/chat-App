cf71dfb6965d23716532807c5da3fb04df380bad
hasithaDa <hamdhasith@gmail.com>
2025-02-28 16:01:55 +0530

    ProfilePage Updated

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -31,7 +31,7 @@ export const useAuthStore = create((set, get) => ({
       toast.success("Account created successfully");
       get().connectSocket();
     } catch (error) {
-      toast.error(error.response.data.message);
+      toast.error(error.response?.data?.message || "An error occurred");
     } finally {
       set({ isSigningUp: false });
     }
